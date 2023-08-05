72281e17896dde97f7d542ce62eb814a6aa1e728
SJFXSTUDIO <ismailhassan@jebat.net>
2023-08-05 14:27:55 +0800

    portfolio ismail

---
+++ isz-portfolio/maklumatstaf.sql
@@ -0,0 +1,10 @@
+/*INSERT INTO maklumatstaf (`namastaf`, `email`, `phoneno`)
+VALUES ('Mohammad Ismail Bin Hassan', 'ismailhassan88@moe.gov.my', '0138825148'),
+('Hasliah Hatirin', 'liaismail88@gmail.com', '0198066310'),
+('Mohammad Rizwan Bin Hassan', 'rizwanhassan@gmail.com', '0138889999');*/ 
+
+/*UPDATE maklumatstaf
+SET `email` = 'kamekpunya@moe.gov.my'
+WHERE id = 5 OR id = 6; */
+
+DELETE FROM maklumatstaf WHERE id = 5;
